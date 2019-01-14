45650ce30a855e44f4598b1173487ef4e1baa8de
Mallaury FIEDLER <mallaury.fiedler@etudiant.univ-reims.fr>
2019-01-14 17:01:57 +0100

    Ajout sprite

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,12 @@
 var game = new Phaser.Game(800,600,Phaser.AUTO,'content',{preload: preload, create: 
-create,update:update});
+create,update:update}); 
 
 function preload(){ 
+game.load.image('pangolin-vie','asset/pangolin-vie.png'); 
 } 
 
 function create(){ 
+monSprite=game.add.sprite(0,0,'pangolin-vie');
 } 
 
 function update(){ 
