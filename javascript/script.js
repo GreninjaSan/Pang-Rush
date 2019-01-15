1f50656d7863edfef13bb8b119c31a16344c2c07
Alban CHAMPAGNE <cham0033@iut-rcc-info.urca>
2019-01-15 18:06:48 +0100

    rajoute de quelques trucs pour le moteur physique

--- javascript/script.js
+++ javascript/script.js
@@ -5,8 +5,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
 var cursors;
 var J1;
 var vitesse = 300;
+var test_desc;
+var aerial;
 function preload(){ 
-    game.load.spritesheet('J1_spr', 'asset/Sprites Joueur1.png', 39, 38);
+    game.load.spritesheet('J1_spr', 'asset/Sprites Joueur1.png', 78, 76);
 } 
 
 function create() {
@@ -22,8 +24,10 @@ function create() {
     //assignement des sprites
     Perso1 = game.add.sprite(0, 200, 'J1_spr');
     Perso1.anchor.setTo(0.5, 0.5);
-    Perso1.animations.add('marche', [1,3], 5, true);
-    
+    Perso1.animations.add('marche_d', [1,3], 5, true);
+    Perso1.animations.add('marche_g', [0, 2], 5, true);
+    Perso1.animations.add('acc_g', [8], 5, true);
+    Perso1.animations.add('acc_d', [9], 5, true);
 
     //allocation du moteur de jeu
     game.physics.enable(Perso1, Phaser.Physics.ARCADE);
@@ -35,13 +39,14 @@ function create() {
 function update() {
     Perso1.body.velocity.x *= 0.90;
     //mouvements
-    if (J1.gauche.isDown) {
-        Perso1.body.velocity.x -= 0.08 * vitesse
 
-    } 
     if (J1.droite.isDown) {
         Perso1.body.velocity.x += 0.08 * vitesse
-        Perso1.play('marche');
+        Perso1.play('marche_d');
+    }
+    else if (J1.gauche.isDown) {
+        Perso1.body.velocity.x -= 0.08 * vitesse
+        Perso1.play('marche_g');
     }
     if (J1.bas.isDown) {
         Perso1.body.velocity.y = vitesse
@@ -50,4 +55,6 @@ function update() {
         Perso1.body.velocity.y = -1.3*vitesse
     }
 
+
+    //if (Perso1.body.velocity.y < 10)
 }
