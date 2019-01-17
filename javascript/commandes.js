956658fd8bd2981c95608869edb706b9fc7566c4
Mallaury FIEDLER <mallaury.fiedler@etudiant.univ-reims.fr>
2019-01-17 16:12:38 +0100

    Commandes

--- javascript/commandes.js
+++ javascript/commandes.js
@@ -4,11 +4,11 @@ var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'content', {
 }); 
 
 function preload(){ 
-    game.load.image('test', 'asset/pangolin-mort.png', 1024, 768);
+    game.load.image('commandes', 'asset/commandes.png', 1024, 768);
 }
 
 function create(){
-    test = game.add.sprite(0,0,'test');
+    test = game.add.sprite(0,0,'commandes');
 }
 
 function update(){
